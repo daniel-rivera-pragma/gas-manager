ee041a77e9d8898f7d63f03aaa54b7228f3a8ee1
Daniel F Rivera C <daniel.rivera@pragma.com.co>
2025-11-03 13:13:15 -0500

    FIX: Ajustar nombre de archivo contenedor de SpreadsheetsManager

--- cmd/add/lib.sh
+++ cmd/add/lib.sh
@@ -16,8 +16,8 @@ create_folder "Lib:./src/lib"
 echo "INF~ Copying JS files" >&2
 declare dependency_manager_js="dependency-manager.js:${root_dir}/templates/lib/dependency-manager.js.sample:./src/lib/dependency-manager.js"
 declare cache_manager_js="cache-manager.js:${root_dir}/templates/lib/cache-manager.js.sample:./src/lib/cache-manager.js"
-declare spreadsheet_manager_js="spreadsheet-manager.js:${root_dir}/templates/lib/spreadsheet-manager.js.sample:./src/lib/spreadsheet-manager.js"
-copy_element "${dependency_manager_js}" "${cache_manager_js}" "${spreadsheet_manager_js}"
+declare spreadsheets_manager_js="spreadsheets-manager.js:${root_dir}/templates/lib/spreadsheets-manager.js.sample:./src/lib/spreadsheets-manager.js"
+copy_element "${dependency_manager_js}" "${cache_manager_js}" "${spreadsheets_manager_js}"
 # Copy type files
 [ ! -d ./types ] && echo "ERR~ No types folder found" >&2 && exit 1
 echo "INF~ Copying type definition files" >&2
