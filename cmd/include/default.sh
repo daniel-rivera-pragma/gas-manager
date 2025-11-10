bbc3fcb767397c750bb00c228a62aec5ba06627a
Daniel F Rivera C <daniel.rivera@pragma.com.co>
2025-11-10 05:44:46 -0500

    ADD: Modificar la utilidad add:lib para transformarla en include, gestionando mejor las librerias

--- cmd/include/default.sh
+++ cmd/include/default.sh
@@ -4,20 +4,24 @@ set -euo pipefail
 declare root_dir="${1}" && shift
 [ ! -f "${root_dir}/functions/load-functions.sh" ] && echo "ERR~ Functions loader not found" >&2 && exit 1
 source "${root_dir}/functions/load-functions.sh"
-load_functions "${root_dir}" "copy-element" "create-folder"
+load_functions "${root_dir}" "copy-element" "create-folder" "map-libraries"
 # Initialize
-echo "INF~ add:lib command started" >&2
+echo "INF~ include command started" >&2
 [ ! -f ./.clasp.json ] && echo "ERR~ No .clasp.json file found" >&2 && exit 1
+# Map libraries
+[ ${#} -eq 0 ] && echo "ERR~ No libraries specified to include" >&2 && exit 1
+declare libraries=$(map_libraries "${root_dir}" "${@}")
+[ -z "${libraries}" ] && echo "ERR~ Unexpected empty libraries map" >&2 && exit 1
 # Scafold lib directories
 [ ! -d ./src ] && echo "ERR~ No src folder found" >&2 && exit 1
-echo "INF~ Scafolding directories" >&2
-create_folder "Lib:./src/lib"
+if [ ! -d ./src/lib ]; then
+  echo "INF~ Scafolding directories" >&2
+  create_folder "Lib:./src/lib"
+fi
 # Copy lib files
 echo "INF~ Copying JS files" >&2
-declare dependency_manager_js="dependency-manager.js:${root_dir}/templates/lib/dependency-manager.js.sample:./src/lib/dependency-manager.js"
-declare cache_manager_js="cache-manager.js:${root_dir}/templates/lib/cache-manager.js.sample:./src/lib/cache-manager.js"
-declare spreadsheets_manager_js="spreadsheets-manager.js:${root_dir}/templates/lib/spreadsheets-manager.js.sample:./src/lib/spreadsheets-manager.js"
-copy_element "${dependency_manager_js}" "${cache_manager_js}" "${spreadsheets_manager_js}"
+declare lib_map=(${libraries})
+copy_element "${lib_map[@]}"
 # Copy type files
 [ ! -d ./types ] && echo "ERR~ No types folder found" >&2 && exit 1
 echo "INF~ Copying type definition files" >&2
