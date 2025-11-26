ab42c8459527154ab72dd1e12e20cec54e4bbbd6
Daniel F Rivera C <daniel.rivera@pragma.com.co>
2025-11-26 09:36:53 -0500

    ADD: Implementar comando para listar los ambientes

---
+++ cmd/env/list.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -euo pipefail
+# bootstrap
+declare root_dir="${1}" && shift
+echo "INF~ env:list command started" >&2
+# list-env-files
+[ ! -d ./.gasm ] && echo "INF~ No envs found" >&2 && exit 0
+declare envs=$(ls ./.gasm)
+[ -z "${envs}" ] && echo "INF~ No envs found" >&2 && exit 0
+echo "INF~ Current available envs" >&2
+for env_name in ${envs}; do
+  echo "     - $(echo "${env_name}" | sed -e "s/clasp-//" -e "s/\.json//")" >&2
+done
+exit 0
